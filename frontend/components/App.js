5d50586ecae0eaab9a80575f0084be10611cbeba
unknown <dshipuk@yahoo.com>
2022-04-11 12:10:45 -0700

    added To Do List, Add Button Clear Button

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state({
+
+    })
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <h1>To Do List</h1>
+        <ul>
+          <li>Go To Gym</li>
+          <li>Eat Dinner</li>
+          <l1>Clean Room</l1>
+        </ul>
+
+        <form>
+          <input 
+          
+          />
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
